78015af2b415d25fbce5381d53409ce90d01d484
Balatskii <u.balatskii@gmail.com>
2021-07-10 19:47:16 +0300

    Function types

--- js-lessons-app/task-function.js
+++ js-lessons-app/task-function.js
@@ -10,3 +10,28 @@ function showFirstMessage() {
 
 showFirstMessage("Hello world");
 console.log(num)
+
+function calc(a, b) {
+    return (a + b);
+}
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+
+const logger = function () {
+    console.log("Hello");
+};
+logger();
+
+const calc = (a, b) => {
+    return a + b
+};
